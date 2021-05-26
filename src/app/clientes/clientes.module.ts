57c916cec00884827d8d211aeda32d466b992964
AlanCastellanosSulub <adjcastellanos@gmail.com>
2021-05-25 21:42:55 -0500

    Second commit

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ClientesService } from './clientes.service';
+import { AltaClienteComponent } from './alta-cliente/alta-cliente.component';
+
+import { FormsModule } from '@angular/forms';
+import { ListadoClientesComponent } from './listado-clientes/listado-clientes.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AltaClienteComponent,
+    ListadoClientesComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
+  ],
+  providers: [
+    ClientesService
+  ],
+  exports: [
+    AltaClienteComponent,
+    ListadoClientesComponent
   ]
 })
 export class ClientesModule { }
